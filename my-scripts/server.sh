2dc912a01ea65288d25420ad17e58b26d291286a
Moremi Vannak <moremi.va@gmail.com>
2026-04-25 07:58:45 +0700

    Move source code to `frontend` dir in preparation for e2e tests

--- my-scripts/server.sh
+++ my-scripts/server.sh
@@ -6,6 +6,6 @@
 gnome-terminal --tab --title="Conduit Backend" --working-directory="/home/rinne/projects/my-package/my-realworld/nitro-prisma-zod-realworld-example-app" -- bash -c "make run; exec bash"
 
 # 2. Frontend: TEA React
-gnome-terminal --tab --title="Conduit Frontend" --working-directory="/home/rinne/projects/my-package/my-realworld/tea-cup-realworld" -- bash -c "npm run dev; exec bash"
+gnome-terminal --tab --title="Conduit Frontend" --working-directory="/home/rinne/projects/my-package/my-realworld/tea-cup-realworld/frontend" -- bash -c "npm run dev; exec bash"
 
 echo "Servers started in new terminal tabs."
